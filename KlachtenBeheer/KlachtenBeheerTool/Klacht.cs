604363296c4b34e7383a3b85727f8ddf9a327b4a
karim076 <59794989+karim076@users.noreply.github.com>
2022-05-10 09:46:54 +0200

    Added Class + names

---
+++ KlachtenBeheer/KlachtenBeheerTool/Klacht.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlachtenBeheerTool
+{
+    public class Klacht
+    {
+        public string Row { get; set; }
+        public string Id { get; set; }
+        public string Source { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string DateOfVisit { get; set; }
+        public string ReviewScore { get; set; }
+        public string Message { get; set; }
+    }
+}
