14bb86b868a2410685ac8ac5eec7f2237f8e1529
karim076 <59794989+karim076@users.noreply.github.com>
2022-05-10 10:20:21 +0200

    Added data template and aligbnment

--- KlachtenBeheer/KlachtenBeheerTool/MainPage.xaml.cs
+++ KlachtenBeheer/KlachtenBeheerTool/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CsvHelper;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
