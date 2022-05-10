d01a65c47de4217648938ee85df3e4529ae0faf2
karim076 <59794989+karim076@users.noreply.github.com>
2022-05-10 09:50:12 +0200

    Added and deleted stuff

--- KlachtenBeheer/KlachtenBeheerTool/MainPage.xaml.cs
+++ KlachtenBeheer/KlachtenBeheerTool/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,9 +29,37 @@ namespace KlachtenBeheerTool
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            var picker = new FileOpenPicker();
+            picker.SuggestedStartLocation = PickerLocationId.Downloads;
+            picker.FileTypeFilter.Add(".csv");
 
+            //TODO: voeg hier je eigen code toe, zoals uit H5, paragraaf 7
+            var file = await picker.PickSingleFileAsync();
+            if (file == null)
+            {
+                tbFileStatus.Text = "Geen geldig bestand gekozen";
+            }
+            else
+            {
+                tbFileStatus.Text = file.Path;
+            }
+
+            using (var fileAcces = await file.OpenReadAsync())
+            {
+                using (var stream = fileAcces.AsStreamForRead())
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                        {
+                            var records = csv.GetRecords<Klacht>();
+                            lvKlacht.ItemsSource = records;
+                        }
+                    }
+                }
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
